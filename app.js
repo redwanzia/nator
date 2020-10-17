0d98ec9a7d26c7cbe7af6fb033a182a6edff74ae
Redwan Zia <redwan.zia@gmail.com>
2020-10-16 21:24:30 -0400

    added first route

--- app.js
+++ app.js
@@ -1,6 +1,10 @@
 const express = require('express');
 const app = express();
 
+app.get('/', (req, res) => {
+  res.status(200).send('hello from the server side');
+});
+
 const port = 5000;
 app.listen(port, () => {
   console.log(`App listening on port ${port}...`);
