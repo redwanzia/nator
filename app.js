5faa85cb6c495ab2d092b52936f8971b854ea186
Redwan Zia <redwan.zia@gmail.com>
2020-10-16 22:20:16 -0400

    fixed minor bugs

--- app.js
+++ app.js
@@ -2,12 +2,14 @@ const fs = require('fs');
 const express = require('express');
 const app = express();
 
-const tours = fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json}`);
+const tours = JSON.parse(
+  fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`)
+);
 
 app.get('/api/v1/tours', (req, res) => {
-  res.status(200).send({
+  res.status(200).json({
     status: 'success',
-    data: { tours }
+    data: { tours: tours }
   });
 });
 
