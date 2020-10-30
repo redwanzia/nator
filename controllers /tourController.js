ee0da4f0c6ede114ba10aa56799bdf7a683130d6
redwan zia <redwan.zia@gmail.com>
2020-10-29 20:52:19 -0400

    added checkBody middleware to post controller

--- controllers /tourController.js
+++ controllers /tourController.js
@@ -16,6 +16,16 @@ exports.checkId = (req, res, next, val) => {
   next();
 };
 
+exports.checkBody = (req, res, next) => {
+  if (!req.body.name || !req.body.price) {
+    return res.status(404).json({
+      status: 'fail',
+      message: 'missing name or price'
+    });
+  }
+  next();
+};
+
 exports.getAllTours = (req, res) => {
   res.status(200).json({
     status: 'success',
