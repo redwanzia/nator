83d4afee250e4d6ad2eb31e7e748ef8f7636bfd8
Redwan Zia <redwan.zia@gmail.com>
2021-03-23 22:20:52 -0400

    added aggrigation pipe line

--- routes/tourRoutes.js
+++ routes/tourRoutes.js
@@ -4,16 +4,10 @@ const tourController = require('../controllers /tourController');
 const router = express.Router();
 // router.param('id', tourController.checkId);
 router.route('/top-5-cheap').get(tourController.aliasTopTour, tourController.getAllTours);
+router.route('/stats').get(tourController.getTourStats);
+router.route('/monthly-plan/:year').get(tourController.getMonthlyPlan);
 
-
-router
-  .route('/')
-  .get(tourController.getAllTours)
-  .post(tourController.createTour);
-router
-  .route('/:id')
-  .get(tourController.getTour)
-  .patch(tourController.updateTour)
-  .delete(tourController.deleteTour);
+router.route('/').get(tourController.getAllTours).post(tourController.createTour);
+router.route('/:id').get(tourController.getTour).patch(tourController.updateTour).delete(tourController.deleteTour);
 
 module.exports = router;
